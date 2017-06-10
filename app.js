9c86be76a8410af38c6ae3d80813d427fd68b097
saksham ahluwalia <sakshamahluwalia5@gmail.com>
2017-06-10 16:21:29 +0000

    adding the url for the database

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10"
+mongoose.connect(url);
 
-mongoose.connect(process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
